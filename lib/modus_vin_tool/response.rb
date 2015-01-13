53854d38f700d5411881bcf98ba0c7df69c8af74
Steve Lewis <steve.lewis@moduspowered.com>
2015-01-13 12:58:08 -0800

    Modify response to accomodate more options
    Now that it is possible to get back compatibility results for more than
    a single device, the wrapper around the raw response needed to be
    modified to make checking compatibility against a specific device more
    convienent.

--- lib/modus_vin_tool/response.rb
+++ lib/modus_vin_tool/response.rb
@@ -7,8 +7,14 @@ module ModusVinTool
       @parsed_response = JSON.parse(raw_response)
     end
 
-    def compatible?
-      @parsed_response['compatible']
+    def compatible?(device_name = nil)
+      return @parsed_response['compatible'] if @parsed_response['device']
+      device_info = @parsed_reponse['devices'].select{|d| d['name'] == device_name }.first
+      return device_info['compatible'] if device_info
+    end
+
+    def incompatible_devices
+      return @parsed_response['devices'].select{|d| !d['compatible'] }.map{|d| d['name'] }
     end
 
     def incompatibility_code
