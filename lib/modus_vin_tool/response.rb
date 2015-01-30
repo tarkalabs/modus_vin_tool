f7395a955fd1361a30bf9905471278f9b05c0456
Gaurav Agarwal <gauravagarwalr@gmail.com>
2015-01-30 10:40:29 -0800

    Adding back the accidental delete! :(

--- lib/modus_vin_tool/response.rb
+++ lib/modus_vin_tool/response.rb
@@ -3,18 +3,20 @@ module ModusVinTool
     attr_reader :raw_response, :parsed_response
 
     def initialize(raw_response)
-      @raw_response = raw_response  
+      @raw_response = raw_response
       @parsed_response = JSON.parse(raw_response)
     end
 
     def compatible?(device_name = nil)
       return @parsed_response['compatible'] if @parsed_response['device']
       device_list = @parsed_response['devices']
+
+      device_info = device_list.select{|d| d['name'] == device_name }.first
       return device_info['compatible'] if device_info
     end
 
     def devices
-      return @parsed_response['devices'] 
+      return @parsed_response['devices']
     end
 
     def incompatible_devices
