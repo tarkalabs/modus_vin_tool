e71b0a4a776186e08d07d140a09ac0f0e4f78bae
Steve Lewis <steve.lewis@moduspowered.com>
2015-06-11 13:58:35 -0700

    Allow for sending requests on behalf of others

--- lib/modus_vin_tool/api.rb
+++ lib/modus_vin_tool/api.rb
@@ -5,20 +5,22 @@ module ModusVinTool
     end
   
     def get_compatibility_info
-      account_name = ModusVinTool.config[:account_name]
-      api_key      = ModusVinTool.config[:api_key]
-      device_name  = @info[:device_name]
-      vin          = @info[:vin]
+      account_name     = ModusVinTool.config[:account_name]
+      for_account_name = @info[:for_account_name]
+      api_key          = ModusVinTool.config[:api_key]
+      device_name      = @info[:device_name]
+      vin              = @info[:vin]
 
-      raw_response = send_request(account_name, device_name, api_key, vin)
+
+      raw_response = send_request(account_name, device_name, api_key, vin, for_account_name)
       response = Response.new(raw_response)
       return response
     end
 
     private
-      def send_request(account_name, device_name, api_key, vin)
+      def send_request(account_name, device_name, api_key, vin, for_account_name)
         uri = URI('http://vin.modustools.com/api/check_compatibility')
-        params = {account_name: account_name, device_name: device_name, api_key: api_key, vin: vin}
+        params = {account_name: account_name, device_name: device_name, api_key: api_key, vin: vin, for_account_name: for_account_name}
         uri.query = URI.encode_www_form(params)
         res = Net::HTTP.get_response(uri)
         return res.body
--- lib/modus_vin_tool/version.rb
+++ lib/modus_vin_tool/version.rb
@@ -1,3 +1,3 @@
 module ModusVinTool
-  VERSION = "0.0.2"
+  VERSION = "0.0.3"
 end
